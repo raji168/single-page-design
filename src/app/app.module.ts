7c249f68947344c448d9811bfdc93340fb5f7897
raji <rajeswarisundar99@gmail.com>
2022-04-01 15:17:16 +0530

    single page alignment commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    IvyCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
